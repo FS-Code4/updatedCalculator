65bf87b16a0d8d5ec509b4ec207b23b1fac99456
Farid Sikander <fscode4@gmail.com>
2025-04-02 09:53:40 -0400

    Initialized variables in global scope, handled operator, number, equals and clear buttons individually in own functions.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,23 @@
-export function calculate(x, y, operator) {
-    let result;
+let result;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+export function calculate(num1, num2, operator) {
+    
 
     switch (operator) {
         case '+':
-            result = x + y;
+            result = num1 + num2;
             break;
         case '-':
-            result = x - y;
+            result = num1 - num2;
             break;
         case '*':
-            result = x * y;
+            result = num1 * num2;
             break;
         case '/':
-            result = x / y;
+            result = num1 / num2;
     }
 
     return result;
@@ -22,6 +27,7 @@ export function calculate(x, y, operator) {
 
 let resultDisplay;
 
+
 function updateDisplay() {
     let buttons = document.querySelectorAll('.js-display');
 
@@ -29,12 +35,13 @@ function updateDisplay() {
 
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            resultDisplay.textContent += button.textContent
+            resultDisplay.textContent += button.textContent;
+
         })
     })
 }
 
-function clear() {
+function clearDisplay() {
     let clear = document.querySelector('.js-clear');
 
     clear.addEventListener('click', () => {
@@ -43,5 +50,55 @@ function clear() {
     })
 }
 
+//
+
+let numbers = document.querySelectorAll('.number');
+let ops = document.querySelectorAll('.operator');
+let equalsButtton = document.querySelector('.js-equals-sign')
+
+//Handle number clicks
+
+
+//Handle operator clicks
+function handleOperatorClick() {
+    ops.forEach((op) => {
+        op.addEventListener('click', () => {
+            operator = op.textContent;
+        })
+    })    
+}
+
+function handleNumberClick() {
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+            if (operator === '') {
+                num1+= number.textContent;
+            } else {
+                num2 += number.textContent;
+            }
+        })
+    })
+}
+
+function equals() {
+    equalsButtton.addEventListener('click', () => {
+        let result = calculate(parseInt(num1), parseInt(num2), operator);
+        console.log(result)
+        resultDisplay.textContent = result;
+    })
+}
+
+handleNumberClick();
+handleOperatorClick();
 updateDisplay();
-clear();
+clearDisplay();
+equals();
+
+//When a number button is pressed, it is assigned to num1
+
+//When an operator button is pressed, it is assigned to operator
+
+
+
+
+
